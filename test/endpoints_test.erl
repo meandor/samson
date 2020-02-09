64ed1794fba9d03f135583c91b984829a6cf4f22
Daniel Schruhl <danielschruhl@gmail.com>
2020-02-09 09:38:05 +0100

    add failing test

--- test/endpoints_test.erl
+++ test/endpoints_test.erl
@@ -7,7 +7,12 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-health_endpoints_test() ->
+health_endpoint_test() ->
   Actual = endpoints:routes(),
   Expected = {"/health", health_handler, []},
+  ?assert(lists:member(Expected, Actual)).
+
+google_chat_endpoint_test() ->
+  Actual = endpoints:routes(),
+  Expected = {"/gchat", google_chat_handler, []},
   ?assert(lists:member(Expected, Actual)).
