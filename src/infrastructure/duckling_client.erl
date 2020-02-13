9847ad6284ca5fa54011d67f20a42aacfbb12402
Daniel Schruhl <danielschruhl@gmail.com>
2020-02-13 15:41:19 +0100

    add metric to observe duckling calls

--- src/infrastructure/duckling_client.erl
+++ src/infrastructure/duckling_client.erl
@@ -5,7 +5,7 @@
 %%%-------------------------------------------------------------------
 -module(duckling_client).
 -behaviour(ner_client).
-
+-include("../metrics_registry_h.hrl").
 -export([recognize_entities/1, extract_entities/1]).
 
 extract_entity_value(Value) ->
@@ -25,14 +25,33 @@ extract_entities(Body) ->
   EntitiesMap = jiffy:decode(Body, [return_maps]),
   lists:map(fun extract_entity/1, EntitiesMap).
 
+call_api(Start, Api, Payload) ->
+  Headers = [{<<"Content-Type">>, <<"application/x-www-form-urlencoded">>}],
+  {ok, StatusCode, _RespHeaders, ClientRef} = hackney:request(post, Api, Headers, Payload, []),
+  prometheus_histogram:observe(?DUCKLING_REQUESTS, [StatusCode], os:system_time() - Start),
+  if
+    StatusCode == 200 ->
+      {ok, Body} = hackney:body(ClientRef),
+      extract_entities(Body);
+    true ->
+      lager:error("Error getting response from Duckling API. Got status code: ~p", [StatusCode]),
+      []
+  end.
+
 -spec recognize_entities(chatbot:message()) -> chatbot:entities().
 recognize_entities(Message) ->
   Locale = "en_GB",
   {ok, DucklingAPI} = application:get_env(samson, duckling_api),
-  Method = post,
-  Headers = [{<<"Content-Type">>, <<"application/x-www-form-urlencoded">>}],
   URLEncodedMessage = http_uri:encode(binary_to_list(Message)),
   Payload = list_to_binary("locale=" ++ Locale ++ "&text=" ++ URLEncodedMessage),
-  {ok, 200, _RespHeaders, ClientRef} = hackney:request(Method, list_to_binary(DucklingAPI), Headers, Payload, []),
-  {ok, Body} = hackney:body(ClientRef),
-  extract_entities(Body).
+  Start = os:system_time(),
+  try
+    call_api(Start, list_to_binary(DucklingAPI), Payload)
+  catch
+    Class:Reason:Stacktrace ->
+      lager:error("Error calling duckling api"),
+      lager:error(
+        "~nStacktrace:~s",
+        [lager:pr_stacktrace(Stacktrace, {Class, Reason})]
+      )
+  end.
