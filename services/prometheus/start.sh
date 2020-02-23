f20d372a84f14dc470af30c0195ea75e094d7990
Daniel Schruhl <danielschruhl@gmail.com>
2020-02-23 14:24:06 +0100

    use random port in prometheus

---
+++ services/prometheus/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+/bin/prometheus \
+    --config.file=/etc/prometheus/prometheus.yml \
+    --storage.tsdb.path=/prometheus \
+    --web.listen-address=:${PORT} \
+    --web.console.libraries=/usr/share/prometheus/console_libraries \
+    --web.console.templates=/usr/share/prometheus/consoles
