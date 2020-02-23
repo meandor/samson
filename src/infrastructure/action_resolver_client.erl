09a0e4521f86f42134c33fd97d15c8e252b5860a
Daniel Schruhl <danielschruhl@gmail.com>
2020-02-23 18:15:08 +0100

    change variable

--- src/infrastructure/action_resolver_client.erl
+++ src/infrastructure/action_resolver_client.erl
@@ -5,4 +5,4 @@
 %%%-------------------------------------------------------------------
 -module(action_resolver_client).
 -callback resolve_action(chatbot:userId(), chatbot:action(), chatbot:entities()) ->
-  NextAction :: chatbot:message().
+  Response :: chatbot:message().
